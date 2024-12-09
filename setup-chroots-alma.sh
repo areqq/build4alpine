7f4056effaab8728bd8dc40b33ca0bd0098f885f
Areq <arek@patyk.net>
2024-12-09 10:40:54 +0100

    - pobieraj najnowszy obraz

--- setup-chroots-alma.sh
+++ setup-chroots-alma.sh
@@ -5,13 +5,18 @@
 
 . "${0%/*}/vars.conf"
 
-URL="https://images.linuxcontainers.org/images/almalinux/9/amd64/default/20241207_23%3A08/rootfs.tar.xz"
+BASE=https://images.linuxcontainers.org/images/almalinux/9/amd64/default/
+
+LAST=$(wget -o /dev/null $BASE -O - | grep href | tail -n1 | awk -F '"' '{print $2}')
+
+URL="${BASE}${LAST}rootfs.tar.xz"
 RPM_libdvbcsa="https://download1.rpmfusion.org/free/el/updates/9/x86_64/l/libdvbcsa-devel-1.1.0-15.el9.x86_64.rpm https://download1.rpmfusion.org/free/el/updates/9/x86_64/l/libdvbcsa-1.1.0-15.el9.x86_64.rpm"
 
 CHROOT_DIR="${BASE_DIR}/alma9-x86_64"
 
 set -e
 echo -e "${yellow}Przygotowuje ${CHROOT_DIR} ${reset}"
+echo $URL
 rm -fr ${CHROOT_DIR}
 mkdir -p $CHROOT_DIR
 cd $CHROOT_DIR
